53f28a0b5c862b04ba52fb12d55a3f1cdfe09a07
Liam Monahan <liam@umiacs.umd.edu>
2015-01-05 12:36:23 -0500

    Allow a ListPack to be indented
    For example, to set the indentation level to 4 characters, construct a
    ListPack as ListPack(..., indentation=4).

--- qav/listpack.py
+++ qav/listpack.py
@@ -6,20 +6,23 @@ class ListPack(object):
     BOLD = '\033[1m'
     OFF = '\033[0m'
 
-    def __init__(self, lp):
-        self.sep = ": "
-        self.buf = "  "
-        self.width = 79
+    def __init__(self, lp, sep=": ", padding="  ", indentation=0, width=79):
+        self.sep = sep
+        self.padding = padding
+        self.indentation = indentation
+        self.width = width
         self._lp = lp
+
+        self.new_line = '' + (' ' * self.indentation)
 
     def calc(self, t):
         s1, s2 = t
-        return len(str(s1)) + len(self.sep) + len(str(s2)) + len(self.buf)
+        return len(str(s1)) + len(self.sep) + len(str(s2)) + len(self.padding)
 
     def bold(self, t):
         s1, s2 = t
         return '%s%s%s%s%s%s' % (self.BOLD, str(s1), self.OFF, self.sep,
-                                 str(s2), self.buf)
+                                 str(s2), self.padding)
 
     def append_item(self, item):
         self._lp.append(item)
@@ -29,16 +32,16 @@ class ListPack(object):
 
     def __str__(self):
         _str = ''
-        line = ''
-        line_length = 0
+        line = self.new_line
+        line_length = len(line)
         for i in self._lp:
             if line_length + self.calc(i) > self.width:
                 if _str != '':
                     _str = _str + '\n' + line
                 else:
                     _str = line
-                line = self.bold(i)
-                line_length = self.calc(i)
+                line = self.new_line + self.bold(i)
+                line_length = len(self.new_line) + self.calc(i)
             else:
                 line += self.bold(i)
                 line_length += self.calc(i)
