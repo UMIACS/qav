9f2a6578272f6701e8189cd5649d874e954feda8
Liam Monahan <liam@liammonahan.com>
2018-03-17 07:49:48 -0400

    Added python3 classifier

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
     ],
     **extra
